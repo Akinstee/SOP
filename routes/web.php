59d402d2be214c210ff1b319fa23057d55308938
Temitayo <akinstemi123@gmail.com>
2023-11-20 18:41:35 +0100

    login modification

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\InstructorController;
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,6 +26,25 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
+//Admin Dashboard Route
+Route::group(['middleware' => ['auth', 'admin']], function () {
+Route::get('/admin/dashboard', 'AdminController@dashboard')->name('admin.dashboard');
+
+Route::group(['middleware' => ['auth', 'admin']], function () {
+    Route::get('/admin/dashboard', 'AdminController@dashboard')->name('admin.dashboard');
+    Route::get('/admin/manage/users', 'AdminController@manageUsers')->name('admin.manage.users');
+    Route::delete('/admin/delete/user/{id}', 'AdminController@deleteUser')->name('admin.delete.user');
+    Route::post('/admin/promote/user/{id}', 'AdminController@promoteUser')->name('admin.promote.user');
+    Route::post('/admin/demote/user/{id}', 'AdminController@demoteUser')->name('admin.demote.user');
+    // Add more routes as needed
+});
+
+
+});
+Route::group(['middleware' => ['auth', 'admin']], function () {
+    Route::get('/admin/dashboard', 'AdminController@dashboard')->name('admin.dashboard');
+    // Add more routes as needed
+});
 
 //Students Registration and login Route
 Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
@@ -31,7 +52,7 @@ Route::post('/students', [StudentController::class, 'store'])->name('students.st
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
-
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 //Instructor Registration and Login Route
 Route::get('/instructor/register', [InstructorController::class, 'showRegistrationForm'])->name('instructor.register');
