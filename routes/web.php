8d13fcb13725d7417bbb783f92f956dece2a812d
coolguythomas <tikukaiwe@gmail.com>
2023-11-20 09:03:31 +0100

    Web update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+<<<<<<< Updated upstream
+=======
+Route::middleware('guest')->prefix('/admin')->name('admin.')->controller(LoginController::class)->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/check', [LoginController::class, 'check'])->name('check');
 });
+
+Route::middleware('guest')->prefix('/user')->name('user.')->controller(LoginController::class)->group(function () {
+    Route::get('/login', [LoginController::class, 'login'])->name('login');
+    Route::post('/check', [LoginController::class, 'check'])->name('check');
+});
+
+//Students Registration and login Route
+Route::middleware('guest')->prefix('/student')->name('student.')->controller(StudentController::class)->group(function () {
+    Route::get('/create', [StudentController::class, 'index'])->name('create');
+    Route::post('/save', [StudentController::class, 'save'])->name('save');
+});
+
+//Instructor Registration and Login Route
+Route::middleware('guest')->prefix('/instructor')->name('instructor.')->controller(LoginController::class)->group(function () {
+    Route::get('/register', [InstructorController::class, 'showRegistrationForm'])->name('instructor.register');
+    Route::post('/', [InstructorController::class, 'register']);
+});
+
+Route::middleware('guest')->prefix('/instructor')->name('instructor.')->controller(InstructorController::class)->group(function () {
+    Route::get('/create', [StudentController::class, 'create'])->name('create');
+    Route::post('/save', [StudentController::class, 'save'])->name('save');
+});
+
+
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+>>>>>>> Stashed changes
