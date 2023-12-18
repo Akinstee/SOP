aaf9578e877dcbf9ce21bb24e96ac20cfe8539a1
Temitayo <akinstemi123@gmail.com>
2023-12-18 01:04:21 +0100

    Notification

--- resources/views/Students/notifications.blade.php
+++ resources/views/Students/notifications.blade.php
@@ -5,66 +5,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- <link rel="stylesheet" href="{{ asset('css/styles.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/notification.css') }}">
     <title>Student Notifications</title>
     <style>
-        body {
-    font-family: 'Arial', sans-serif;
-    margin: 0;
-    padding: 0;
-    background-color: #f0f0f0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-}
-
-.notification-container {
-    position: fixed;
-    top: 20px;
-    right: 20px;
-    max-width: 300px;
-}
-
-.notification {
-    padding: 15px;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 10px;
-}
-
-.notification.success {
-    background-color: #4caf50; /* Green */
-    color: #fff;
-}
-
-.notification.error {
-    background-color: #f44336; /* Red */
-    color: #fff;
-}
-
-.close-btn {
-    background: none;
-    border: none;
-    color: #fff;
-    font-size: 18px;
-    cursor: pointer;
-}
+        
     </style>
 </head>
 <body>
-
-    <div class="notification-container">
-        @foreach ($notifications as $notification)
-            <div class="notification {{ $notification->type }}" data-notification-id="{{ $notification->id }}">
-                <p>{{ $notification->message }}</p>
-                <button class="close-btn" onclick="deleteNotification({{ $notification->id }})">&times;</button>
-            </div>
-        @endforeach
-        <button class="close-bar-btn" onclick="closeNotificationBar()">&times;</button>
+    <div class="container">
+        @include('layouts.sidebars')
+        <div class="notification-container">
+            @foreach ($notifications as $notification)
+                <div class="notification {{ $notification->type }}" data-notification-id="{{ $notification->id }}">
+                    <p>{{ $notification->message }}</p>
+                    <button class="close-btn" onclick="deleteNotification({{ $notification->id }})">&times;</button>
+                </div>
+            @endforeach
+            <button class="close-bar-btn" onclick="closeNotificationBar()">&times;</button>
+        </div>
     </div>
 
     <script>
