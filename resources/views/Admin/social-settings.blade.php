3ca0cb9b319c4453a78bebc042a47d2e8b28c285
Temitayo <akinstemi123@gmail.com>
2024-01-06 11:18:25 +0100

    social-settings

--- resources/views/Admin/social-settings.blade.php
+++ resources/views/Admin/social-settings.blade.php
@@ -6,9 +6,9 @@
       name="viewport"
       content="width=device-width, initial-scale=1.0, user-scalable=0"
     />
-    <title>Preskool - Books</title>
+    <title>SOPS - Social-Settings</title>
 
-    <link rel="shortcut icon" href="assets/img/favicon.png" />
+    <link rel="shortcut icon" href="{{ asset('/img/SOPS.png')}}" />
 
     <link
       href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap"
@@ -17,24 +17,24 @@
 
     <link
       rel="stylesheet"
-      href="assets/plugins/bootstrap/css/bootstrap.min.css"
+      href="{{ asset('/plugins/bootstrap/css/bootstrap.min.css')}}"
     />
 
-    <link rel="stylesheet" href="assets/plugins/feather/feather.css" />
+    <link rel="stylesheet" href="{{ asset('/plugins/feather/feather.css')}}" />
 
-    <link rel="stylesheet" href="assets/plugins/icons/flags/flags.css" />
+    <link rel="stylesheet" href="{{ asset('/plugins/icons/flags/flags.css')}}" />
 
-    <link rel="stylesheet" href="assets/css/bootstrap-datetimepicker.min.css" />
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap-datetimepicker.min.css')}}" />
 
     <link
       rel="stylesheet"
-      href="assets/plugins/fontawesome/css/fontawesome.min.css"
+      href="{{ asset('/plugins/fontawesome/css/fontawesome.min.css')}}"
     />
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/all.min.css')}}" />
 
-    <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css" />
+    <link rel="stylesheet" href="{{ asset('/plugins/select2/css/select2.min.css')}}" />
 
-    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="{{ asset('/css/style32.css"')}} />
   </head>
   <body>
     <div class="main-wrapper">
