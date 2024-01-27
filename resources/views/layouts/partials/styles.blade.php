1d421403e979128043e4afad04fee4ac6a8ff640
kingdanie <daniedemola@gmail.com>
2024-01-27 22:03:16 +0100

    added new partials for student dashboard  sections

---
+++ resources/views/layouts/partials/styles.blade.php
@@ -0,0 +1,12 @@
+<link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/plugins/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('/plugins/feather/feather.css')}}">
+    <link rel="stylesheet" href="{{ asset('/plugins/icons/flags/flags.css')}}">
+    <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('/plugins/simple-calendar/simple-calendar.css')}}">
+    <link rel="stylesheet" href="{{ asset('/css/style32.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
