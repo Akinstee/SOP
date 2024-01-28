bff676aa4866301d13a8b8d4c01510339aeb7e88
kingdanie <daniedemola@gmail.com>
2024-01-28 18:33:58 +0100

    adding extra menu to the instructor's sidebar

--- resources/views/layouts/instructorside.blade.php
+++ resources/views/layouts/instructorside.blade.php
@@ -5,20 +5,21 @@
                 <li class="menu-title">
                     <span>Main Menu</span>
                 </li>
-                <li class="submenu active">
-                    <a href="{{ route('instructor.dashboard') }}">Dashboard</a><i class="feather-grid"></i> <span> Instructor</span></a>
-                    
+                <li class="submenu {{ is_active_link('instructor/dashboard*') }}">
+                    <a href="{{ route('instructor.dashboard') }}">
+                        <i class="feather-grid"></i>
+                         <span> Instructor Dashboard</span>
+                    </a>
                 </li>
-                <li class="submenu">
+                <li class="submenu {{ is_active_link('instructor/courses*') }}">
                     <a href="#"><i class="fas fa-book-reader"></i> <span> Courses</span> <span
                             class="menu-arrow"></span></a>
                     <ul>
                         <li><a href="{{ route('instructor.courses')}}">Courses List</a></li>
-                        <li><a href="{{ route('instructor.courses')}}">Courses Add</a></li>
-                        <li><a href="{{ route('instructor.edit-course') }}">Courses Edit</a></li>
+                        <li><a href="{{ route('instructor.add-course')}}">Courses Add</a></li>
                     </ul>
                 </li>
-                <li class="submenu">
+                <li class="submenu {{ is_active_link('instructor/invoices*') }}">
                     <a href="#"><i class="fas fa-clipboard"></i> <span> Invoices</span> <span
                             class="menu-arrow"></span></a>
                     <ul>
@@ -29,7 +30,10 @@
                         <li><a href="{{ route('instructor.invoices-settings') }}">Invoice Settings</a></li>
                     </ul>
                 </li>
-                <li class="submenu">
+                <li class="{{ is_active_link('instructor/profile*') }}">
+                    <a href="{{ route('instructor.profile')}}"><i class="fas fa-user"></i> <span> Profile</span></a>
+                </li>
+                <li class="submenu {{ is_active_link('instructor/settings*') }}">
                     <a href="{{ route('instructor.settings')}}"><i class="fas fa-shield-alt"></i> <span> Settings </span> <span
                             class="menu-arrow"></span></a>
                     <ul>
