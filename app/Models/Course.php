5f3a962b8e7e26f97016660c1d755ae1cc521177
kingdanie <daniedemola@gmail.com>
2024-01-29 00:35:02 +0100

    updated course model to align with migration fields

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    protected $fillable = ['course_id', 'course_name', 'course_author', 'course_rating', 'course_price'];
+    protected $fillable = ['name', 'author', 'rating', 'price'];
 }
